17b2decc18b2a4715207c2350367be8c1b1061a6
prasanna.natarajan <prasanna.natarajan@lookout.com>
2024-08-29 07:50:17 +0530

    first commit

---
+++ stub_player_store.go
@@ -0,0 +1,23 @@
+package poker
+
+type StubPlayerStore struct {
+	scores   map[string]int
+	winCalls []string
+	league   League
+}
+
+func (st *StubPlayerStore) GetPlayerScore(player string) int {
+	score := st.scores[player]
+	return score
+}
+
+func (st *StubPlayerStore) RecordWin(player string) {
+	st.winCalls = append(st.winCalls, player)
+}
+
+func (st *StubPlayerStore) RemovePlayer(name string) {
+}
+
+func (st *StubPlayerStore) GetLeague() League {
+	return st.league
+}
